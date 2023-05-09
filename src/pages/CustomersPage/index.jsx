5b7147d730fc42d0ddaf6e6a9d7dac1dad629f72
Mohamad Abdalrazak <xxiimonsteriixx20@gmail.com>
2023-05-09 12:19:41 +0300

    fix customers decimal

--- src/pages/CustomersPage/index.jsx
+++ src/pages/CustomersPage/index.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
-import MyTable from "../../components/MyTable";
-import PageLayout from "../../components/PageLayout";
-import MyDialog from "../../components/MyDialog";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import useTable from "../../hooks/useTable";
-import { useSelector } from "react-redux";
-import * as Yup from "yup";
+import React, { useEffect, useState } from 'react';
+import MyTable from '../../components/MyTable';
+import PageLayout from '../../components/PageLayout';
+import MyDialog from '../../components/MyDialog';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import useTable from '../../hooks/useTable';
+import { useSelector } from 'react-redux';
+import * as Yup from 'yup';
 import {
   addCustomer,
   addUser,
@@ -15,87 +15,87 @@ import {
   editCustomer,
   editUser,
   getUsers,
-} from "../../store/users";
-import { useDispatch } from "react-redux";
-import PageHeading from "../../components/PageHeading";
-import CustomTableHeading from "../../components/CustomTableHeading";
-const PAGE_TITLE = "الزبائن";
+} from '../../store/users';
+import { useDispatch } from 'react-redux';
+import PageHeading from '../../components/PageHeading';
+import CustomTableHeading from '../../components/CustomTableHeading';
+const PAGE_TITLE = 'الزبائن';
 const COLUMNS = [
   {
-    id: "id",
-    label: "رقم الحساب",
+    id: 'id',
+    label: 'رقم الحساب',
     minWidth: 80,
-    align: "center",
+    align: 'center',
     isField: false,
   },
   {
-    id: "name",
-    label: "اسم الحساب",
+    id: 'name',
+    label: 'اسم الحساب',
     minWidth: 150,
-    align: "left",
+    align: 'left',
     isField: true,
     required: true,
     isLink: true,
-    urlPath: "shops",
+    urlPath: 'shops',
   },
   {
-    id: "mobileNumber",
-    label: "رقم الهاتف المحمول",
+    id: 'mobileNumber',
+    label: 'رقم الهاتف المحمول',
     minWidth: 150,
-    align: "center",
+    align: 'center',
     isField: true,
     required: true,
   },
   {
-    id: "address",
-    label: "العنوان",
+    id: 'address',
+    label: 'العنوان',
     minWidth: 100,
-    align: "center",
+    align: 'center',
     isField: true,
   },
   {
-    id: "note",
-    label: "ملاحظة",
+    id: 'note',
+    label: 'ملاحظة',
     minWidth: 170,
-    align: "left",
+    align: 'left',
     isField: true,
   },
   {
-    id: "accountBalance",
-    label: "صافي الحساب",
+    id: 'accountBalance',
+    label: 'صافي الحساب',
     minWidth: 50,
-    align: "center",
+    align: 'center',
     isField: true,
-    format: (value) => value.toLocaleString("en-US"),
+    format: (value) => value.toLocaleString('en-US'),
     isMoney: true,
   },
   {
-    id: "accountBalanceValues",
-    label: "قيمة صافي الحساب",
+    id: 'accountBalanceValues',
+    label: 'قيمة صافي الحساب',
     minWidth: 100,
-    align: "center",
+    align: 'center',
     isField: true,
-    format: (value) => value.toLocaleString("en-US"),
+    format: (value) => value.toLocaleString('en-US'),
     isMoney: true,
   },
 ];
 
 const DAILY_ROW_INTIAL_VALUE = {
-  id: "",
-  name: "",
-  mobileNumber: "",
-  address: "",
-  note: "",
+  id: '',
+  name: '',
+  mobileNumber: '',
+  address: '',
+  note: '',
   accountBalance: 0,
   accountBalanceValues: 0,
 };
 const validationSchema = Yup.object({
-  name: Yup.string().required("يرجى إدخال اسم الحساب"),
-  mobileNumber: Yup.number().required("يرجى إدخال رقم الموبايل"),
+  name: Yup.string().required('يرجى إدخال اسم الحساب'),
+  mobileNumber: Yup.number().required('يرجى إدخال رقم الموبايل'),
   address: Yup.string(),
-  accountBalance: Yup.number().required("يرجى كتابة صافي الحساب"),
+  accountBalance: Yup.number().required('يرجى كتابة صافي الحساب'),
   note: Yup.string(),
-  accountBalanceValues: Yup.number().required("يرجى كتابة قيمة صافي الحساب"),
+  accountBalanceValues: Yup.number().required('يرجى كتابة قيمة صافي الحساب'),
 });
 const DailyPage = () => {
   const { dialog, setDialog, handleOpenAddDialog } = useTable(
@@ -103,9 +103,15 @@ const DailyPage = () => {
   );
   const dispatch = useDispatch();
   const users = useSelector((state) => state.users);
-  const rows = users.data.filter((customer) => customer.userType === "زبون");
+  let rows = users.data.filter((customer) => customer.userType === 'زبون');
+  rows = rows.map((row) => ({
+    ...row,
+    accountBalance: row.accountBalance.toFixed(2),
+    accountBalanceValues: row.accountBalanceValues.toFixed(2),
+  }));
+
   const dispatchers = {
-    add: (customer) => dispatch(addUser({ ...customer, userType: "زبون" })),
+    add: (customer) => dispatch(addUser({ ...customer, userType: 'زبون' })),
     edit: (editedCustomer) => dispatch(editUser({ ...editedCustomer })),
     delete: (customerId) => dispatch(deleteUser(customerId)),
   };
@@ -122,7 +128,7 @@ const DailyPage = () => {
     { value: 0, values: 0 }
   );
   const total = {
-    title: "الاجمالي",
+    title: 'الاجمالي',
     ...totalMoney,
   };
   return (
@@ -141,10 +147,10 @@ const DailyPage = () => {
         <Button
           variant="contained"
           sx={{
-            height: "60px",
-            margin: "2rem auto",
-            display: "block",
-            width: "50%",
+            height: '60px',
+            margin: '2rem auto',
+            display: 'block',
+            width: '50%',
           }}
           color="error"
           onClick={handleOpenAddDialog}
